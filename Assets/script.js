d8ad12f2f7b8e01156a40070284643cb84b9f4be
Uriel <CarrilloUriel81@gmail.com>
2023-10-01 21:08:56 -0700

    Starting JS code wind temp and humidity now working when inputing city

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,22 @@
-/* Restarting As I'd Like to freshly practice and understanding how to make my API Function with JavaScript*/
+const apiKey = "d4e9f0c9a253b69ab2eeb89bc3cfb9c7";
+
+function findWeather(city) {
+  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('cityName').textContent = data.name;
+      document.getElementById('temperature').textContent = data.main.temp;
+      document.getElementById('wind').textContent = data.wind.speed;
+      document.getElementById('humidity').textContent = data.main.humidity;
+    })
+    .catch((err) => {
+      console.error("Uh oh, no weather fetch!", err);
+    });
+}
+
+document.getElementById("searchBtn").addEventListener("click", function () {
+  const cityInput = document.getElementById("cityInput").value;
+  findWeather(cityInput);
+});
