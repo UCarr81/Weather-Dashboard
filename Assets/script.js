ec82b5f68fc506365428cf94ac021c15b36d9592
Uriel <CarrilloUriel81@gmail.com>
2024-01-10 20:40:32 -0800

    Added CSS and Weather Icons

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 const apiKey = "d4e9f0c9a253b69ab2eeb89bc3cfb9c7";
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const defaultCity = 'Portland';
 
 // Function to find weather based on city
 //function findWeather(city) {
@@ -52,14 +53,20 @@ function updateCurrentWeather(data) {
   const temperatureElement = document.getElementById('temperature');
   const windElement = document.getElementById('wind');
   const humidityElement = document.getElementById('humidity');
+  const weatherIconElement = document.getElementById('weatherIcon');
 
   const city = data.name;
   const temperatureKelvin = data.main.temp;
   const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9/5 + 32;
   const windSpeed = data.wind.speed;
   const humidity = data.main.humidity;
+  const weatherIconCode = data.weather[0].icon;
 
-  cityNameElement.textContent = `Current Weather in ${city}`;
+  const currentDate = moment().format('MM/DD/YYYY');
+
+  weatherIconElement.src = `http://openweathermap.org/img/w/${weatherIconCode}.png`;
+
+  cityNameElement.textContent = `Current Weather in ${city} (${currentDate})`;
   temperatureElement.textContent = `Temperature: ${temperatureFahrenheit.toFixed(2)} °F`;
   windElement.textContent = `Wind: ${windSpeed} MPH`;
   humidityElement.textContent = `Humidity: ${humidity}%`;
@@ -71,13 +78,15 @@ function updateForecast(data) {
 
   forecastContainer.innerHTML = '';
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 40; i+= 8) {
     const forecast = data.list[i];
-    const date = moment(forecast.dt_txt).format('MM/DD/YYYY');
+    const date = moment(forecast.dt_txt).add(1, 'days').format('MM/DD/YYYY');
     const temperatureKelvin = forecast.main.temp;
     const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9/5 + 32;
     const windSpeed = forecast.wind.speed;
     const humidity = forecast.main.humidity;
+    const weatherIconCode = forecast.weather[0].icon;
+    
 
     const card = document.createElement('div');
     card.classList.add('card');
@@ -85,6 +94,7 @@ function updateForecast(data) {
     card.innerHTML = `
             <div class="card-body">
                 <h5 class="card-title">${date}</h5>
+                <img src="http://openweathermap.org/img/w/${weatherIconCode}.png" alt="Weather Icon">
                 <p class="card-text">Temperature: ${temperatureFahrenheit.toFixed(2)} °F</p>
                 <p class="card-text">Wind: ${windSpeed} MPH</p>
                 <p class="card-text">Humidity: ${humidity}%</p>
@@ -96,3 +106,7 @@ function updateForecast(data) {
 }
 
 document.getElementById('search-form').addEventListener('submit', handleFormSubmit);
+
+document.addEventListener('DOMContentLoaded', function () {
+  getWeatherData(defaultCity);
+});
